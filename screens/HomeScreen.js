8153b81f6826d078d47c988d0bfbd63a57b33b7a
Artem Tyshchuk <artemtyschukk@gmail.com>
2024-04-14 19:40:35 +0200

    starter

---
+++ screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React from "react";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView className="bg-white h-full ">
+      <View className="p-5">
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
